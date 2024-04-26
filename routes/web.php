c6cad5ab9e05cd93936302af747d667b488511c4
dedy270895 <dedospratamos@gmailcom>
2024-04-27 00:45:47 +0700

    fortify, login, logout

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages.dashboard',['type_menu' => 'dashboard']);
+    return view('pages.auth.auth-login',['type_menu' => 'dashboard']);
 });
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard',['type_menu' => 'home']);
+    })->name('home');
+});
+
